965211eb255ba355be77405c605b991152f986ec
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-11-18 14:24:58 +0100

    Update UserController.java

--- hamNews-main/src/main/java/com/hamNews/Controler/UserController.java
+++ hamNews-main/src/main/java/com/hamNews/Controler/UserController.java
@@ -1,4 +1,57 @@
 package com.hamNews.Controler;
 
+import com.hamNews.Model.DB.DatabaseConnection;
+import com.hamNews.Model.User.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserController {
+
+    public boolean registerUser(String firstName, String lastName, String password, String email) {
+        String insertUserSQL = "INSERT INTO Users (firstName, lastName, password, email) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(insertUserSQL)) {
+
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, password);
+            preparedStatement.setString(4, email);
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    public User loginUser(String email, String password) {
+        String selectUserSQL = "SELECT * FROM Users WHERE email = ? AND password = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(selectUserSQL)) {
+
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                int userId = resultSet.getInt("userId");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                return new User(userId, firstName, lastName, password, email, null, null);
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
