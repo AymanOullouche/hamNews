6aa10ac3cbcc9b8b7b8b235d41060b25fc634ea9
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-11-20 22:07:57 +0100

    Update ArticleListView.java

--- hamNews-main/src/main/java/com/hamNews/Vue/ArticleListView.java
+++ hamNews-main/src/main/java/com/hamNews/Vue/ArticleListView.java
@@ -3,14 +3,18 @@ package com.hamNews.Vue;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
-public class ArticleListView extends Application {
+public class ArticleDetailView extends Application {
 
     @Override
     public void start(Stage primaryStage) {
@@ -19,29 +23,34 @@ public class ArticleListView extends Application {
         banner.setPrefHeight(100);
         banner.setAlignment(Pos.CENTER_LEFT);
 
-        Text userText = new Text("Bonjour Walid");
-        userText.setFill(Color.WHITE);
-        userText.setFont(Font.font("Arial", 20));
-        banner.getChildren().add(userText);
-        HBox.setMargin(userText, new javafx.geometry.Insets(0, 0, 0, 20));
+        Image appImage = new Image("com/hamNews/Vue/images/Profile.png");
+        ImageView appImageView = new ImageView(appImage);
+        appImageView.setFitWidth(100);
+        appImageView.setFitHeight(100);
+        appImageView.setPreserveRatio(true);
+        appImageView.setTranslateY(-130);
 
         Circle circle = new Circle(90);
         circle.setFill(Color.rgb(52, 152, 219));
         circle.setTranslateY(-130);
 
-        Text articleText = new Text("Liste des article");
+        StackPane circleWithImage = new StackPane();
+        circleWithImage.getChildren().addAll(circle, appImageView);
+
+        Text articleText = new Text("article Détail");
         articleText.setFont(Font.font("Arial", 24));
         articleText.setFill(Color.BLACK);
 
-        VBox content = new VBox(20, circle, articleText);
+
+        VBox content = new VBox(20, circleWithImage, articleText);
         content.setAlignment(Pos.TOP_CENTER);
         content.setStyle("-fx-background-color: #ecf0f1;");
         content.setPrefHeight(400);
 
         VBox root = new VBox(banner, content);
 
-        Scene scene = new Scene(root, 800, 600);
-        primaryStage.setTitle("Interface principale");
+        Scene scene = new Scene(root, 1050, 550);
+        primaryStage.setTitle("Détail de article");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
