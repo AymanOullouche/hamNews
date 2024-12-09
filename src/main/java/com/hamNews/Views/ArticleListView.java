8dfb1b5797783a9d57a46f5f485d6c91736a9165
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-12-09 15:45:21 +0100

    Update ArticleListView.java

--- src/main/java/com/hamNews/Views/ArticleListView.java
+++ src/main/java/com/hamNews/Views/ArticleListView.java
@@ -3,6 +3,8 @@ package com.hamNews.Views;
 import com.hamNews.Controler.ArticleController;
 import com.hamNews.Controler.NewsScraperTask;
 import com.hamNews.Model.Article.ArticleSelect;
+import com.hamNews.Model.DB.Session;
+import com.hamNews.Model.User.User;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,6 +14,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -19,130 +23,115 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.input.ScrollEvent;
-/*import com.twelvemonkeys.imageio.plugins.webp.WebPImageReaderSpi;
-import javax.imageio.ImageIO;*/
-
-
-
-import com.hamNews.Model.Article.Article;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.hamNews.Views.ArticleView.openSignUpFormWithAnimation;
 
 public class ArticleListView extends Application {
 
     private List<ArticleSelect> articles = new ArrayList<>();
-    private HBox articlesContainer = new HBox(20);
-    private int currentIndex = 0;
-    private static final int ARTICLES_PER_ROW = 3;
+    private HBox articlesContainer = new HBox(25);
+    private int currentIndex = 0; // Indice des articles chargés
+    private static final int ARTICLES_PER_ROW = 4; // Nombre d'articles à afficher par ligne
     private ScrollPane scrollPane;
-    public  ArticleSelect article;
-    private Button readMoreButton;
-
+    private Button readMoreButton ;
+    private Button likeButton ;
+    private Button downloadButton;
+    private User theUser;
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) {}
 
 
-    }
-
-    // Méthode pour afficher les articles
     private void displayArticles() {
-
+        theUser = Session.getLoggedInUser();
         articlesContainer.setAlignment(Pos.CENTER);
         articlesContainer.setStyle("-fx-background-color: white; -fx-padding: 15;");
-        // Clear the container before adding new articles
         articlesContainer.getChildren().clear();
 
-        // Afficher une ligne d'articles
         for (int i = currentIndex; i < Math.min(currentIndex + ARTICLES_PER_ROW, articles.size()); i++) {
             ArticleSelect article = articles.get(i);
 
-            // Conteneur de la carte
-            VBox card = new VBox(10); // Espacement entre les éléments de la carte
+            VBox card = new VBox(20);
             card.setPadding(new Insets(10));
-            // Style mis à jour : pas de bordure et fond blanc
             card.setStyle("-fx-background-color: white; -fx-border-width: 0; -fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.2), 5, 0, 0, 0);");
-            card.setPrefWidth(250); // Largeur fixe de chaque carte
+            card.setPrefWidth(100);
 
-            // Image de l'article
             ImageView imageView = new ImageView(new Image(article.getImageUrl()));
-            imageView.setFitHeight(150);
-            imageView.setFitWidth(250);
+            imageView.setFitWidth(232);
+            imageView.setFitHeight(80);
             imageView.setPreserveRatio(true);
 
-            // Titre de l'article
             Label title = new Label(article.getTitle());
             title.setStyle("-fx-font-size: 16px; -fx-font-weight: bold; -fx-text-fill: black;");
-            title.setMaxWidth(250);
-            title.setMaxHeight(60);
+            title.setMaxWidth(232);
+            title.setMaxHeight(80);
             title.setWrapText(true);
-            title.setAlignment(Pos.CENTER); // Centrer le titre
+            title.setAlignment(Pos.CENTER);
 
-
-            // Résumé (description de l'article)
             Label snippet = new Label(article.getDescription());
             snippet.setWrapText(true);
             snippet.setStyle("-fx-text-fill: #666; -fx-font-size: 14px;");
-            snippet.setMaxWidth(250); // Largeur maximale
-            snippet.setMaxHeight(60); // Hauteur maximale pour le champ de description
+            snippet.setMaxWidth(232);
+            snippet.setMaxHeight(60);
             snippet.setStyle("-fx-text-overrun: ellipsis; -fx-wrap-text: true; -fx-text-fill: #666;");
-
 
             HBox actions = new HBox(10);
             actions.setAlignment(Pos.CENTER);
 
-            Button likeButton = new Button();
-            likeButton.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-cursor: hand;");  // Cache la bordure du bouton
+            likeButton = new Button();
+            likeButton.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-cursor: hand;");
             likeButton.setMinSize(40, 40);
 
-            Button downloadButton = new Button();
+            downloadButton = new Button();
             downloadButton.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-cursor: hand;");
             downloadButton.setMinSize(40, 40);
 
-            Image Danger = new Image(getClass().getResource("/com/hamNews/Views/images/Like.png").toExternalForm());
-            ImageView dangericon = new ImageView(Danger);
-            dangericon.setFitWidth(20);
-            dangericon.setFitHeight(20);
-            likeButton.setGraphic(dangericon);
+            Image IconI = new Image(getClass().getResource("/com/hamNews/Views/images/Like.png").toExternalForm());
+            ImageView ImageIcon = new ImageView(IconI);
+            ImageIcon.setFitWidth(20);
+            ImageIcon.setFitHeight(20);
+            likeButton.setGraphic(ImageIcon);
 
-            Image downloadIcon = new Image(getClass().getResource("/com/hamNews/Views/images/Donwload.png").toExternalForm());
-            ImageView downloadImage = new ImageView(downloadIcon);
-            downloadImage.setFitWidth(20);
-            downloadImage.setFitHeight(20);
-            downloadButton.setGraphic(downloadImage);  // Ajoutez l'icône au bouton
+            Image IconD = new Image(getClass().getResource("/com/hamNews/Views/images/Donwload.png").toExternalForm());
+            ImageView downloadIcon = new ImageView(IconD);
+            downloadIcon.setFitHeight(20);
+            downloadIcon.setFitWidth(20);
+            downloadButton.setGraphic(downloadIcon);
 
-// Bouton "Read More"
-            readMoreButton = new Button("Read More");
+            readMoreButton  = new Button("Read More");
             readMoreButton.setStyle("-fx-background-color: transparent; -fx-border-color: transparent; -fx-cursor: hand;");
             readMoreButton.setMinSize(100, 40);
 
-// Ajouter des actions sur les boutons
+
             likeButton.setOnAction(e -> {
                 System.out.println("Liked article: " + article.getTitle());
+
             });
 
             downloadButton.setOnAction(e -> {
-                article.setDownloaded(!article.isDownloaded());
-                // Change l'icône ou le texte si nécessaire (en fonction de l'état de téléchargement)
-                if (article.isDownloaded()) {
+                if (theUser==null){
 
-                    Image RemoveIcon = new Image(getClass().getResource("/com/hamNews/Views/images/Remove.png").toExternalForm());
-                    ImageView RemoveImage = new ImageView(RemoveIcon);
-                    RemoveImage.setFitWidth(20);
-                    RemoveImage.setFitHeight(20);
-                    downloadButton.setGraphic(RemoveImage);
+                    openBienvenue();
+                }else {
+                    article.setDownloaded(!article.isDownloaded());
+                    if (article.isDownloaded()) {
+                        downloadIcon.setImage(new Image(getClass().getResource("/com/hamNews/Views/images/Remove.png").toExternalForm()));
+                    } else {
+                        downloadIcon.setImage(new Image(getClass().getResource("/com/hamNews/Views/images/Download.png").toExternalForm()));
+                    }
+                    System.out.println("Article " + article.getUrl() + " " + (article.isDownloaded() ? "saved for" : "removed from") + " offline reading");
+                }});
 
-                } else {
+            readMoreButton.setOnAction(e -> {
+                if (theUser==null){
 
-                    downloadImage.setImage(new Image("file:/D:/ENSIASD/TP S1/java/projet2/telecharger.png")); // Rechangez l'icône pour l'icône "Download"
+                    openBienvenue();
+                }else {
+
+                    System.out.println("Read more: " + article.getTitle());
+                    ArticleView.openDetailFormWithAnimation(article);
                 }
-                System.out.println("Article " + article.getUrl() + " " + (article.isDownloaded() ? "saved for" : "removed from") + " offline reading");
             });
-
-
 
 
             actions.getChildren().addAll(likeButton, downloadButton, readMoreButton);
@@ -153,18 +142,9 @@ public class ArticleListView extends Application {
             VBox.setVgrow(space1, Priority.ALWAYS);
             VBox.setVgrow(space2, Priority.ALWAYS);
 
-
-            // Ajouter les éléments à la carte
             card.getChildren().addAll(imageView, title, space1, snippet, space2, actions);
-
-            // Ajouter la carte au conteneur
             articlesContainer.getChildren().add(card);
-            readMoreButton.setOnAction(e -> {
-                System.out.println("Read more: " + article.getTitle());
 
-                ArticleDetailView articleDetailView = new ArticleDetailView(article);
-                openSignUpFormWithAnimation(articleDetailView);
-            });
         }
     }
 
@@ -172,37 +152,59 @@ public class ArticleListView extends Application {
         loadArticles();
 
         scrollPane = new ScrollPane(articlesContainer);
-        scrollPane.setFitToWidth(true);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setStyle("-fx-background-color: white; -fx-border-color: transparent; -fx-padding: 0;");
-
         scrollPane.setOnScroll(event -> handleScroll(event));
+        scrollPane.setOnKeyPressed(event -> handleKeyPress(event));
 
         return scrollPane;
+
+    }
+
+    public void handleScroll(ScrollEvent event) {
+
+        if (event.getDeltaY() < 0) {
+            if (currentIndex + ARTICLES_PER_ROW < articles.size()) {
+                currentIndex += ARTICLES_PER_ROW;
+                displayArticles();
+            }
+        } else if (event.getDeltaY() > 0) {
+            if (currentIndex - ARTICLES_PER_ROW >= 0) {
+                currentIndex -= ARTICLES_PER_ROW;
+                displayArticles();
+            }
+        }
 
 
     }
 
-    // Méthode pour gérer le défilement
-    public void handleScroll(ScrollEvent event) {
-        if (event.getDeltaY() < 0) { // L'utilisateur défile vers le bas
+    public void handleKeyPress(KeyEvent event) {
+        if (event.getCode() == KeyCode.DOWN) {
             if (currentIndex + ARTICLES_PER_ROW < articles.size()) {
-                currentIndex += ARTICLES_PER_ROW; // Charger les articles suivants
+                currentIndex += ARTICLES_PER_ROW;
                 displayArticles();
             }
-        } else if (event.getDeltaY() > 0) { // L'utilisateur défile vers le haut
+        } else if (event.getCode() == KeyCode.UP) {
             if (currentIndex - ARTICLES_PER_ROW >= 0) {
-                currentIndex -= ARTICLES_PER_ROW; // Charger les articles précédents
+                currentIndex -= ARTICLES_PER_ROW;
                 displayArticles();
             }
         }
     }
+    public void openBienvenue() {
+        System.out.println("Ouverture de l'interface Home...");
+
+        Bienvenue bienvenue = new Bienvenue();
+        Stage bienvenueStage = new Stage();
+        bienvenue.start(bienvenueStage);
+        bienvenueStage.show();
+    }
 
     private void loadArticles() {
-
         ArticleController art = new ArticleController();
         articles = art.getArticles();
         displayArticles();
     }
-
 
 }
