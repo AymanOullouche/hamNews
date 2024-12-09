232a33704640851809f43f90f9c1d2718a68d83b
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-12-09 15:39:39 +0100

    Update Dashboard.java

--- src/main/java/com/hamNews/Views/Dashboard.java
+++ src/main/java/com/hamNews/Views/Dashboard.java
@@ -1,6 +1,8 @@
 package com.hamNews.Views;
 
 import com.hamNews.Model.DB.Session;
+import com.hamNews.Model.User.User;
+import javafx.animation.PauseTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Node;
@@ -17,35 +19,48 @@ public class  Dashboard extends Application {
 
     private ArticleView ArticleView;
     private SideBar NewsAggApp;
-    private com.hamNews.Views.SearchBar sideBar;
+    private SearchBar sideBar;
     private ProfileSettingsView profil;
-
     private VBox Articleview;
     private HBox SearchBar;
     private VBox NavBar;
     private ScrollPane profileSettings;
-
     private BorderPane mainLayout = new BorderPane();
-    private Stage primaryStage; // Stocke le Stage principal
+    private Stage primaryStage;
+    private User theUser;
+
 
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage; // Initialiser le Stage principal
-
+        this.primaryStage = primaryStage;
+        theUser = Session.getLoggedInUser();
         ArticleView = new ArticleView();
         NewsAggApp = new SideBar();
         sideBar = new SearchBar();
         profil = new ProfileSettingsView();
 
+        theUser = Session.getLoggedInUser();
+
         Articleview = ArticleView.ShowArticleView();
         NavBar = NewsAggApp.AfficherNavBar();
         SearchBar = sideBar.ShowSideBar();
-        profileSettings = profil.getProfileSettings();
+        WindowManager.addWindow(primaryStage);
+        if (theUser!=null){
 
-        // Boutons et leurs actions
-        NewsAggApp.getProfileButton().setOnAction(e -> openInterfaceWithAnimation(profileSettings));
-        NewsAggApp.getHomeButton().setOnAction(e -> openInterfaceWithAnimation(Articleview));
-        NewsAggApp.getLogoutButton().setOnAction(e -> SeDeconnecter());
+            profil = new ProfileSettingsView();
+
+            profileSettings = profil.getProfileSettings();
+
+            NewsAggApp.getProfileButton().setOnAction(e -> openInterfaceWithAnimation(profileSettings));
+            NewsAggApp.getHomeButton().setOnAction(e -> openInterfaceWithAnimation(Articleview));
+            NewsAggApp.getLogoutButton().setOnAction(e -> SeDeconnecter());}
+        if(theUser==null){
+
+            openBienvenue();
+
+        }
+
+
 
         // Configuration du layout principal
         mainLayout.setTop(SearchBar);
@@ -60,6 +75,8 @@ public class  Dashboard extends Application {
         Image icon = new Image(getClass().getResource("/com/hamNews/Views/images/ConnectPlease.png").toExternalForm());
         primaryStage.getIcons().add(icon);
         primaryStage.show();
+
+
     }
 
     public void openInterfaceWithAnimation(Node argument) {
@@ -76,20 +93,34 @@ public class  Dashboard extends Application {
     }
 
     private void SeDeconnecter() {
-        Session.logout(); // Déconnexion
-        openLogin(); // Ouvrir la page de connexion
+        Session.logout();
+        openLogin();
     }
 
     private void openLogin() {
-        primaryStage.close(); // Fermer l'interface actuelle
+        primaryStage.close();
         System.out.println("Ouverture de l'interface Authentification...");
         Authentification authentification = new Authentification();
         Stage loginStage = new Stage();
-        authentification.start(loginStage); // Démarrer l'interface de connexion
+        authentification.start(loginStage);
         loginStage.show();
     }
+    public void openBienvenue() {
+        System.out.println("Ouverture de l'interface Home après 10 secondes...");
 
+        PauseTransition pause = new PauseTransition(Duration.seconds(10));
+
+        pause.setOnFinished(event -> {
+            Bienvenue bienvenue = new Bienvenue();
+            Stage bienvenueStage = new Stage();
+            bienvenue.start(bienvenueStage);
+            bienvenueStage.show();
+        });
+
+        pause.play();
+    }
     public static void main(String[] args) {
         launch(args);
     }
 }
+
