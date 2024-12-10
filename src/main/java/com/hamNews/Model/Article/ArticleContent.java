afa01a6f329e9100960687c622b89002742d0d36
Moutaoikil_Mohamed <153822288+MoutaoikilMohamed@users.noreply.github.com>
2024-12-10 13:06:22 +0100

    Update ArticleContent.java

--- src/main/java/com/hamNews/Model/Article/ArticleContent.java
+++ src/main/java/com/hamNews/Model/Article/ArticleContent.java
@@ -13,4 +13,3 @@ public class ArticleContent {
 
 
 }
-//"https://ilide.info/docgeneratev2?fileurl=https%3A%2F%2Fscribd.vdownloaders.com%2Fpdownload%2F659135907%2FCours-M233-Complet-MoumniMohammed233&title=Cours+M233+Complet+MoumniMohammed&utm_source=scrfree&utm_medium=queue&utm_campaign=dl "
